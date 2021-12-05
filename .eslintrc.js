59f2f950889e1c71edd03a1341cfb03951977065
hyoni0817 <dudu1104@naver.com>
2021-12-06 00:53:29 +0900

    프론트엔드 개발 환경 세팅 (#1)
    * docs: branch 보호 정책 작업 내용 추가
    
    * Add: package.json
    
    * Add: next, react, react-dom
    
    * Add: node_modules 폴더를 포함한 .gitignore
    
    * Add: typescript
    
    * Add: React와 Node의 타입 선언
    
    * Add: eslint
    
    * Add: .eslintrc.js와 eslint 플러그인
    
    * Add: settings 추가해서 react 버전 설정 및 배열 포맷 변경
    
    - vscode에 설치된 prettier 플러그인으로 인해 배열 포맷이 변경됨.
    
    * Add: prettier
    
    * Add: eslint-config-prettier
    
    - 이 플러그인 설치 후 .eslintrc.js의 extends에도 prettier 추가함.
    
    * Add: .prettierrc
    
    * Add: Home 페이지 생성
    
    * Add: next 실행을 위해 npm scripts 커스텀 정의
    
    * Chore: js에서 tsx로 파일 확장자를 변경
    
    * Add: next 실행 후 생성된 파일
    
    - tsconfig.json은 이 프로젝트에서 타입스크립트가 사용된다는 것을 감지하여 tsconfig.json이 자동으로 생성됨.
    
    * Add: next 실행 후 생성된 .next 폴더
    
    * Fix: eslint 에러 해결을 위해 rules 추가

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'arrow-body-style': ['error', 'always'],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx'] }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
